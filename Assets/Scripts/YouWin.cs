d948d1dfda25b2a1d632007717145cc585e0ab73
Whitt <whittsanders@gmail.com>
2023-04-02 07:02:33 -0400

    Update YouWin.cs

--- Assets/Scripts/YouWin.cs
+++ Assets/Scripts/YouWin.cs
@@ -7,12 +7,13 @@ public class YouWin : MonoBehaviour
 {
     
     public string destination;
+    public float duration;
     private void  LoadGame() => SceneManager.LoadScene(destination);
     
     // Start is called before the first frame update
     void Start()
     {
-        Invoke(nameof(LoadGame), 5f);
+        Invoke(nameof(LoadGame), duration);
     }
 
     // Update is called once per frame
